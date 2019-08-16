19162d3e0ec35c632aba4ca6a35b9c08428c611e
José Carlos PHP <38591375+josecarlosphp@users.noreply.github.com>
2019-08-16 18:41:03 +0200

    Create App.php
    Webservice application.

---
+++ src/App.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace josecarlosphp\easyservice;
+
+require_once 'vendor/josecarlosphp/functions/src/internet.php';
+require_once 'vendor/josecarlosphp/functions/src/arrays.php';
+require_once 'vendor/josecarlosphp/functions/src/files.php';
+
+require_once '_inc/classes/MySession.class.php';
+include_once '_inc/classes/MyServiceResponse.class.php';
+
+class App
+{
+    private $debug;
+
+    private $dirSess = '_sess';
+    private $dirLog = '_log';
+    private $dirDebug = '_debug';
+
+    public function __construct($debug=false, $config=array())
+    {
+        $this->debug($debug);
+
+        foreach($config as $key=>$val)
+        {
+            if(property_exists($this, $key))
+            {
+                $this->$key = $val;
+            }
+        }
+
+        $arr = get_object_vars($this);
+        foreach($arr as $key=>$val)
+        {
+            if(mb_substr($key, 0, 3) == 'dir')
+            {
+                $this->$key = ponerBarra($val);
+                if(!is_dir($this->$key) && makeDir($this->$key))
+                {
+                    file_put_contents($this->$key.'.htaccess', 'Deny from all');
+                }
+            }
+        }
+    }
+
+    public static function getDefaultConfig()
+    {
+        return array(
+            'tokenlifetime'=>90,
+            'tokenlenght'=>64,
+            );
+    }
+
+    public function run()
+    {
+        global $app; //Para que esté disponible, por ejemplo para $app->debugging()
+
+        if(empty($app))
+        {
+            $app = $this; //Por si la variable se ha declarado con otro nombre
+        }
+
+        $this->logging('$_REQUEST = '.var_export($_REQUEST, true));
+
+        $q = isset($_GET['q']) ? LimpiarData($_GET['q']) : '';
+        $action = isset($_GET['action']) ? LimpiarData($_GET['action']) : '';
+        $params = $_REQUEST; //$_POST
+
+        $headers = getallheaders();
+        $token = isset($headers['Ocp-Apim-Subscription-Key']) ? $headers['Ocp-Apim-Subscription-Key'] : (isset($params['token']) ? LimpiarData($params['token']) : '');
+
+        $this->debugging('get', $_GET);
+        $this->debugging('post', $_POST);
+        $this->debugging('headers', $headers);
+
+        if($q && $action)
+        {
+            if(is_dir('q/'.$q))
+            {
+                if(is_file('q/'.$q.'/'.$action.'.php'))
+                {
+                    $filename = 'q/'.$q.'/_inc/functions.inc.php';
+                    if(is_file($filename))
+                    {
+                        include $filename;
+                    }
+
+                    $config = self::getDefaultConfig();
+
+                    $filename = 'q/'.$q.'/config.yml';
+                    if(is_file($filename))
+                    {
+                        $aux = self::yamlRead($filename);
+                        foreach($aux as $key=>$val)
+                        {
+                            if(array_key_exists($key, $config))
+                            {
+                                $config[$key] = $val;
+                            }
+                        }
+                    }
+
+                    \MySession::Init(ponerBarra(getcwd().'/'.$this->dirSess).$q.'/', $config['tokenlifetime'], $config['tokenlifetime'] == 0 ? 0 : 1, 1, $config['tokenlenght']);
+
+                    switch($action)
+                    {
+                        case 'Open':
+                            $session = new \MySession();
+                            $token = $session->GetId();
+                            break;
+                        case 'Close':
+                            if(!\MySession::Destroy($token))
+                            {
+                                $this->doResult(\MyServiceResponse::STATUS_ERROR, MySession::Exists($token) ? 'Can not destroy session' : 'Bad token');
+                            }
+                            break;
+                        default:
+                            $session = new \MySession();
+                            if($session->Load($token) === false || $session->Get('sessid') != $token)
+                            {
+                                $this->doResult(\MyServiceResponse::STATUS_ERROR, 'Bad token: '.$token);
+                            }
+                            break;
+                    }
+
+                    include 'q/'.$q.'/'.$action.'.php';
+
+                    switch($action)
+                    {
+                        case 'Open':
+                            $session->Set($token, 'sessid');
+                            $app->doResult(\MyServiceResponse::STATUS_OK, $token);
+                            break;
+                        case 'Close':
+                            $app->doResult(\MyServiceResponse::STATUS_OK, null);
+                            break;
+                        default:
+                            $app->doResult(\MyServiceResponse::STATUS_ERROR, 'Unexpected error');
+                            break;
+                    }
+                }
+                else
+                {
+                    $this->doResult(\MyServiceResponse::STATUS_ERROR, 'Unknown action: '.$action);
+                }
+            }
+
+            http_response_code(404);
+            exit;
+        }
+
+        http_response_code(503);
+        exit;
+    }
+
+    protected function doResult($status, $content)
+    {
+        $this->logging('[Result: '.$status.'] '.var_export($content, true), 'general');
+        header('Content-Type: application/json');
+        echo json_encode(array(
+            'status'=>$status,
+            'content'=>$content,
+        ));
+        exit;
+    }
+
+    public function debug($debug=null)
+    {
+        if(!is_null($debug))
+        {
+            $this->debug = $debug ? true : false;
+        }
+
+        return $this->debug;
+    }
+
+    public function debugging($q, $var)
+    {
+        if($this->debug)
+        {
+            file_put_contents($this->dirDebug.$q, var_export($var, true));
+        }
+    }
+
+    public function logging($str, $q='general')
+    {
+        $fp = fopen($this->dirLog.$q.'.'.date('Y-m').'.log', 'a');
+        fwrite($fp, sprintf('[%s] - %s%s', date('Y-m-d H:i:s'), $str, "\n"));
+        fclose($fp);
+    }
+
+    public static function yamlRead($filename)
+    {
+        return \Spyc::YAMLLoad($filename);
+    }
+
+    public static function yamlWrite($filename, $data)
+    {
+        return file_put_contents($filename, \Spyc::YAMLDump($data, 4, 0, true));
+    }
+}
